76123629e778a09d31157277e96469c44915a3f3
Suresh Kalisetti <kalisettisuresh@gmail.com>
2023-07-04 21:44:55 +0100

    install portainer

--- install-docker.sh
+++ install-docker.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 curl -sSL https://get.docker.com | sh || error "Failed to install Docker."
 sudo usermod -aG docker $USER || error "Failed to add user to the Docker usergroup."
 echo "Remember to reboot for the changes to take effect."
