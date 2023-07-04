1266b8aeaa66d8a7d5cc2ed0574200ef65a61791
Suresh Kalisetti <kalisettisuresh@gmail.com>
2023-07-04 21:49:14 +0100

    fix bash path

--- install-docker.sh
+++ install-docker.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 curl -sSL https://get.docker.com | sh || error "Failed to install Docker."
 sudo usermod -aG docker $USER || error "Failed to add user to the Docker usergroup."
